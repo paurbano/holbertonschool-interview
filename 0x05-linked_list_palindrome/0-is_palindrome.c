abd569649a9fde2dba64068da219ba8d1fef0e47
Pablo <paurbano@gmail.com>
2020-08-27 00:20:40 +0000

    is palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -8,20 +8,23 @@
 int is_palindrome(listint_t **head)
 {
 	/*listint_t *h;*/
-	int palindrome[1024];
-	int i, j, n = 0;
+	int palindrome[2048];
+	int i, j, num = 0;
 
-	if (*head == NULL)
+	if ((*head) == NULL || head == NULL)
+		return (1);
+
+	if ((*head)->next == NULL)
 		return (1);
 
 	while (*head != NULL)
 	{
-		palindrome[n] = (*head)->n;
-		n++;
+		palindrome[num] = (*head)->n;
+		num++;
 		*head = (*head)->next;
 	}
 
-	for (i = 0, j = n - 1 ; i != (n / 2); i++, j--)
+	for (i = 0, j = num - 1 ; i < (num / 2); i++, j--)
 	{
 		if (palindrome[i] != palindrome[j])
 			return (0);
