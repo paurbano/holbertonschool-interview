1714413f7f6e6e12e3fe530934a1ed6fd93a1fab
Pablo <paurbano@gmail.com>
2020-09-02 16:42:38 +0000

    change catch keyboard interruption and create print function

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,10 +2,15 @@
 '''log parsing '''
 
 import sys
-from signal import signal, SIGINT
+# from signal import signal, SIGINT
 
 
 if __name__ == '__main__':
+    status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+              "404": 0, "405": 0, "500": 0}
+    file_size = 0
+    log_parsing = ""
+    '''
     def handler(signal_received, frame):
         # Handle any cleanup here
         # file_size = file_size + int(line_splited[len(line_splited)-1])
@@ -14,14 +19,18 @@ if __name__ == '__main__':
         for key, value in sorted(status.items()):
             if value > 0:
                 print("{}: {}".format(key, value))
-        sys.exit(1)
+        sys.exit(0)
+    '''
+    def print_msg():
+        log_parsing = "File size: {}".format(file_size)
+        print(log_parsing)
+        # print(status, flush=True)
+        for key, value in sorted(status.items()):
+            if value > 0:
+                print("{}: {}".format(key, value))
 
-    signal(SIGINT, handler)
-    status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-              "404": 0, "405": 0, "500": 0}
     countline = 0
-    file_size = 0
-    log_parsing = ""
+    # signal(SIGINT, handler)
     try:
         for line in sys.stdin:
             line_splited = line.split(" ")
@@ -33,14 +42,18 @@ if __name__ == '__main__':
             # increment number of lines readed
             countline += 1
             # every 10 lines print info
-            if countline % 10 == 0 and countline != 0:
+            if countline % 10 == 0:
+                '''
                 log_parsing = "File size: {}".format(file_size)
                 print(log_parsing, flush=True)
                 # print(status, flush=True)
                 for key, value in sorted(status.items()):
                     if value > 0:
                         print("{}: {}".format(key, value))
-        sys.stdout.flush()
-    except(BrokenPipeError, IOError):
-        pass
-    sys.stderr.close()
+                '''
+                print_msg()
+        # sys.stdout.flush()
+    except KeyboardInterrupt:  # (BrokenPipeError, IOError):
+        # pass
+        print_msg()
+        raise
