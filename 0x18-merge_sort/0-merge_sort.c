3dcdc979e9f7738a33ba6542af05016d1c9523c6
Pablo <paurbano@gmail.com>
2021-02-17 06:21:54 +0000

    merge sort

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * merge - merge two arrays
+ *
+ * @a: final array
+ * @n: Number of elements in @array
+ * @m: number of half elements in @array
+ */
+void merge(int *a, int n, int m)
+{
+	int i, j, k;
+	int *x = malloc(n * sizeof(int));
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(a, m);
+	printf("[right]: ");
+	print_array(a + m, n - m);
+	for (i = 0, j = m, k = 0; k < n; k++)
+	{
+		x[k] = j == n      ? a[i++]
+				: i == m      ? a[j++]
+				: a[j] < a[i] ? a[j++]
+				:               a[i++];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = x[i];
+	}
+	printf("[Done]: ");
+	print_array(a, n);
+	free(x);
+}
+/**
+ * merge_sort - Order an array of integers
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void merge_sort(int *array, size_t size)
+{
+	int m;
+
+	if (size < 2)
+		return;
+	m = size / 2;
+	merge_sort(array, m);
+	merge_sort(array + m, size - m);
+	merge(array, size, m);
+}
---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef MERGE_SORT
+#define MERGE_SORT
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+void merge (int *a, int n, int m);
+void merge_sort(int *array, size_t size);
+#endif
