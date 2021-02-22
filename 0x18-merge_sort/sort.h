3713426f7710e4c239fae911e846c6fb6dfc1d5e
Pablo <paurbano@gmail.com>
2021-02-22 20:03:50 +0000

    merge sort

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -3,8 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 100
 
-void print_array(const int *array, size_t size);
 void merge(int *a, int n, int m);
 void merge_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+/*
+void merge(int *array, int *left, int *right, size_t size);
+void merge_sort(int *array, size_t size);
+*/
 #endif
